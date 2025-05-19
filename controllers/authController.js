9c149faeb8b2923017180a733495e74bdef77473
2fira-SE <loarifse16@gmail.com>
2025-05-20 01:05:35 +0300

    fixed auth controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const User = require("../models/user.js");
+const User = require("../models/User");
 const generateToken = require("../lib/utils");
 
 const dotevn = require("dotenv");
